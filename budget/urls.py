e8dabf029f42320826d78e11a4edab4b75f8974e
PeiYee88 <77477618+PeiYee88@users.noreply.github.com>
2024-03-07 02:14:34 +0800

    Add project functionality and connect it to model

--- budget/urls.py
+++ budget/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='list'),
-    path('<slug:project_slug>/', views.project_details, name='detail') 
+    path('add', views.ProjectCreateView.as_view(), name='add'),
+    path('<slug:project_slug>/', views.project_details, name='detail'),
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project
+from .models import Project, Category
+from django.views.generic import CreateView
+from django.utils.text import slugify
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -12,3 +15,25 @@ def project_details(request, project_slug):
         return render(request, 'budget/project-details.html',
                       {'project': project, 'expense_list': project.expenses.all()})
 
+class ProjectCreateView(CreateView):
+      model = Project
+      template_name = 'budget/add-project.html'
+      fields = ('name', 'budget')
+
+      def form_valid(self, form):
+            self.object = form.save(commit=False)
+            self.object.save()
+
+            categories = self.request.POST['categoriesString'].split(',')
+            for category in categories:
+                  Category.objects.create(
+                        project = Project.objects.get(id=self.object.id),
+                        name=category
+                  ).save()
+
+            return HttpResponseRedirect(self.get_success_url())
+      
+      def get_success_url(self):
+            return slugify(self.request.POST['name'])
+
+    
